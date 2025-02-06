6f9f61c670825078e6ddf0e7d135f499fa492c8c
Yuri <yuri.jesus@globmail.com.br>
2025-02-06 17:15:42 -0300

    improvements for mobile

--- src/views/meals-calculator/index.tsx
+++ src/views/meals-calculator/index.tsx
@@ -100,93 +100,60 @@ function MealsCalculator() {
     setMealsList(newMealsList)
     _calculateTotals(newMealsList)
   }
-
+// py-32 sm:py-48 lg:py-56
   return (
-    <div className="relative isolate px-6 pt-14 lg:px-8">
-      <div className="mx-auto max-w-[60%] py-32 sm:py-48 lg:py-56">
-        <div>
-          <div className="text-center inline-block w-[100%]">
-            <div>
+    <div className="relative isolate px-0 md:px-6 pt-2">
+      <div className="mx-0 md:mx-auto max-w-[100%] md:max-w-[60%]">
+        <div className="text-center inline-block w-[100%]">
+          <div>
 
-              Meals Calculator<br></br>
+            Meals Calculator<br></br>
 
-              {/* {foodsList.map((food: any) => {
-                return <FoodItem food={food} ></FoodItem>
-              })} */}
-              <br></br><br></br>
+            {/* {foodsList.map((food: any) => {
+              return <FoodItem food={food} ></FoodItem>
+            })} */}
+            <br></br><br></br>
+            <div className="border">
+              Peso Atual: {userInfo.bodyWeight}<br></br>
+              Metas: <br></br>
+              <div className="w-full">
+                <div className="w-[50%] inline-block">Calorias: 2000</div>
+                <div className="w-[50%] inline-block">Proteinas: {userInfo.bodyWeight * userInfo.proteinGoal}</div>
+                <div className="w-[50%] inline-block">Carboidratos: {userInfo.bodyWeight * userInfo.carbohydrateGoal}</div>
+                <div className="w-[50%] inline-block">Gordura: {userInfo.bodyWeight * userInfo.fatGoal}</div>
+              </div>
               <div className="border">
-                Peso Atual: {userInfo.bodyWeight}<br></br>
-                Metas: <br></br>
+                Total hoje:<br></br>
                 <div className="w-full">
-                  <div className="w-[50%] inline-block">Calorias: 2000</div>
-                  <div className="w-[50%] inline-block">Proteinas: {userInfo.bodyWeight * userInfo.proteinGoal}</div>
-                  <div className="w-[50%] inline-block">Carboidratos: {userInfo.bodyWeight * userInfo.carbohydrateGoal}</div>
-                  <div className="w-[50%] inline-block">Gordura: {userInfo.bodyWeight * userInfo.fatGoal}</div>
-                </div>
-                <div className="border">
-                  Total hoje:<br></br>
-                  <div className="w-full">
-                    <div className="w-[50%] inline-block">Calorias: {totals.calories.toFixed(2)} </div>
-                    <div className="w-[50%] inline-block">Proteínas: {totals.proteins.toFixed(2)}</div>
-                    <div className="w-[50%] inline-block">Carboidratos: {totals.carbohydrate.toFixed(2)}</div>
-                    <div className="w-[50%] inline-block">Gordura: {totals.fat.toFixed(2)}</div>
-                  </div>
+                  <div className="w-[50%] inline-block">Calorias: {totals.calories.toFixed(2)} </div>
+                  <div className="w-[50%] inline-block">Proteínas: {totals.proteins.toFixed(2)}</div>
+                  <div className="w-[50%] inline-block">Carboidratos: {totals.carbohydrate.toFixed(2)}</div>
+                  <div className="w-[50%] inline-block">Gordura: {totals.fat.toFixed(2)}</div>
                 </div>
               </div>
+            </div>
+            <br></br>
+            <div className="border text-black p-[10px] rounded-sm">
               <br></br>
-              <div className="border text-black p-[10px] rounded-sm">
-                <br></br>
-                <div>
-                  <input className="border mb-[10px]" placeholder="Data" type="date" name="date" onChange={_onChangeDate} value={dateSelected}/>
-                </div>
-                <div className="">
-                  Lista:<br></br>
-                  {mealsList.map((meal, index) => (
-                    <div className="border-2 border-indigo-500 rounded-sm my-[20px] p-[5px]">
-                      <div className="w-[50%] inline-block align-top">
-                        Refeição: {index + 1}<br></br>
-                        <div className="mb-[20px]">
-                          <Select
-                            className="w-[300px] inline-block"
-                            value={meal.selectedValue}
-                            onChange={(selected) => _onChangeMeal(selected, index)}
-                            options={_getMealsOptions(foodsList)}
-                          />
-                          <button className="rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2" onClick={() => _addFood(index)}>Adicionar Comida</button>
-                        </div>
-                        <div className="border rounded-sm p-[2px] text-sm">
-                          <table>
-                            <tr className="border">
-                              <th className="border">Nome</th>
-                              <th className="border">Descrição</th>
-                              <th className="border">Quantidade</th>
-                              <th className="border">Calorias</th>
-                              <th className="border">Proteínas</th>
-                              <th className="border">Carboidratos</th>
-                              <th className="border">Gorduras</th>
-                              <th className="border">Ações</th>
-                            </tr>
-                            {/* <div className="border border-green-500"> */}
-                              {meal.items.map((item, foodIndex) => (
-                                <tr className="border">
-                                  <td className="border">{item.name}</td>
-                                  <td className="border">{item.description}</td>
-                                  <td className="border">{item.quantity} ({item.measure})</td>
-                                  <td className="border">{item.calories}</td>
-                                  <td className="border">{item.proteins}</td>
-                                  <td className="border">{item.carbohydrate}</td>
-                                  <td className="border">{item.fat}</td>
-                                  <td className="border"><TrashIcon className="cursor-pointer text-red-500" onClick={() => _onDeleteFood(index, foodIndex)} /></td>
-                                </tr>
-                              ))}
-                            {/* </div> */}
-                          </table>
-                        </div>
+              <div>
+                <input className="border mb-[10px]" placeholder="Data" type="date" name="date" onChange={_onChangeDate} value={dateSelected}/>
+              </div>
+              <div className="">
+                Lista:<br></br>
+                {mealsList.map((meal, index) => (
+                  <div className="border-2 border-indigo-500 rounded-sm my-[20px] p-[5px]">
+                    <div className="w-[100%] md:w-[50%] inline-block align-top">
+                      Refeição: {index + 1}<br></br>
+                      <div className="md:mb-[20px]">
+                        <Select
+                          className="w-[100%] md:w-[300px] inline-block"
+                          value={meal.selectedValue}
+                          onChange={(selected) => _onChangeMeal(selected, index)}
+                          options={_getMealsOptions(foodsList)}
+                        />
+                        <button className="rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2 mt-[20px] md:mt-[0px]" onClick={() => _addFood(index)}>Adicionar Comida</button>
                       </div>
-                      <div className="inline-block w-[50%] mt-[10px]">
-                        <div className="border rounded-sm p-[18px]">
-                          Informações do alimento
-                        </div>
+                      <div className="md:hidden inline-block w-[100%] md:w-[50%] md:mb-[20px]">
                         {meal.selectedValue && <div className="border-1 border-indigo-500 rounded-sm mt-[10px]">
                           Nome: {meal.selectedValue.name} | Descrição: {meal.selectedValue.description}<br></br>
                           Calorias: { meal.selectedValue.calories} | Carboidratos: {meal.selectedValue.carbohydrate} | Proteínas: {meal.selectedValue.proteins}
@@ -215,12 +182,72 @@ function MealsCalculator() {
                           </div>
                         </div>}
                       </div>
-                      {/* <hr className="my-[10px]"></hr> */}
+                      <div className="border rounded-sm p-[2px] text-sm">
+                        <table>
+                          <tr className="border">
+                            <th className="border">Nome</th>
+                            <th className="border">Descrição</th>
+                            <th className="border">Quantidade</th>
+                            <th className="border">Calorias</th>
+                            <th className="border">Proteínas</th>
+                            <th className="border">Carboidratos</th>
+                            <th className="border">Gorduras</th>
+                            <th className="border">Ações</th>
+                          </tr>
+                          {/* <div className="border border-green-500"> */}
+                            {meal.items.map((item, foodIndex) => (
+                              <tr className="border">
+                                <td className="border">{item.name}</td>
+                                <td className="border">{item.description}</td>
+                                <td className="border">{item.quantity} ({item.measure})</td>
+                                <td className="border">{item.calories}</td>
+                                <td className="border">{item.proteins}</td>
+                                <td className="border">{item.carbohydrate}</td>
+                                <td className="border">{item.fat}</td>
+                                <td className="border"><TrashIcon className="cursor-pointer text-red-500" onClick={() => _onDeleteFood(index, foodIndex)} /></td>
+                              </tr>
+                            ))}
+                          {/* </div> */}
+                        </table>
+                      </div>
                     </div>
-                  ))}	
-                </div>
-                <button className="rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2 mt-[30px] mb-[30px]" onClick={_addMeal}>Adicionar Refeição</button>
+                    <div className="hidden md:inline-block w-[100%] md:w-[50%] mt-[10px]">
+                      <div className="border rounded-sm p-[18px]">
+                        Informações do alimento
+                      </div>
+                      {meal.selectedValue && <div className="border-1 border-indigo-500 rounded-sm mt-[10px]">
+                        Nome: {meal.selectedValue.name} | Descrição: {meal.selectedValue.description}<br></br>
+                        Calorias: { meal.selectedValue.calories} | Carboidratos: {meal.selectedValue.carbohydrate} | Proteínas: {meal.selectedValue.proteins}
+                        | Gorduras: {meal.selectedValue.fat} | Quantidade: {meal.selectedValue.quantity} | Medida: {meal.selectedValue.measure}<br></br>
+                        {meal.selectedValue.name !== 'Personalizado' && <div><input className="w-[100px] border" placeholder='Quantidade' onChange={(event) => _onChangeQuantity(event, index)} />({meal.selectedValue.measure})</div>}<br></br>
+                        {meal.selectedValue.name === 'Personalizado' && <div>
+                          <div className="w-[50%] inline-block">
+                            Calorias: <input className="w-[100px] border" placeholder="Quantidade" type="number" name="calories" onChange={(event) => _onChangeCustom(event, index)} />
+                          </div>
+                          <div className="w-[50%] inline-block">
+                            Proteínas: <input className="w-[100px] border" placeholder="Quantidade" type="number" name="proteins" onChange={(event) => _onChangeCustom(event, index)} />
+                          </div>
+                          <div className="w-[50%] inline-block">
+                            Carboidratos: <input className="w-[100px] border" placeholder="Quantidade" type="number" name="carbohydrate" onChange={(event) => _onChangeCustom(event, index)} />
+                          </div>
+                          <div className="w-[50%] inline-block">
+                            Gorduras: <input className="w-[100px] border" placeholder="Quantidade" type="number" name="fat" onChange={(event) => _onChangeCustom(event, index)} />
+                          </div>
+                        </div>}
+                        <div>
+                          Será Ingerido:<br></br>
+                          Calorias: {meal.form.calories} | 
+                          Proteinas: {meal.form.proteins} | 
+                          Carboidratos: {meal.form.carbohydrate} | 
+                          Gordura: {meal.form.fat}
+                        </div>
+                      </div>}
+                    </div>
+                    {/* <hr className="my-[10px]"></hr> */}
+                  </div>
+                ))}	
               </div>
+              <button className="rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2 mt-[30px] mb-[30px]" onClick={_addMeal}>Adicionar Refeição</button>
             </div>
           </div>
         </div>
